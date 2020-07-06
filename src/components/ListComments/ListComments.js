172ba048e022255476224039eb21013b4b866ef1
Radosław <hasiuk9@gmail.com>
2020-07-06 19:13:35 +0200

    avatar

--- src/components/ListComments/ListComments.js
+++ src/components/ListComments/ListComments.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Button, Comment, Form, Header } from "semantic-ui-react";
+import { Button, Comment, Form, Header, Divider } from "semantic-ui-react";
 import styles from "./listComments.modules.scss";
+import Avatar from "react-avatar";
+
 const ListComments = (props) => {
   const [data, setData] = useState([]);
 
@@ -26,8 +28,29 @@ const ListComments = (props) => {
           {item.photo === props.imgURL && (
             <Comment className={styles.comment}>
               <Comment.Content>
-                <Comment.Author as="a">{item.user}</Comment.Author>
-                <Comment.Text>{item.text}</Comment.Text>
+                <Avatar
+                  name={item.user
+                    .split(" ")
+                    .map((n) => n[0])
+                    .join(".")}
+                  className={styles.avatar}
+                  size="30px"
+                />
+                <Comment.Author
+                  as="a"
+                  style={{ marginLeft: "5px", fontSize: "17px" }}
+                >
+                  {item.user}
+                </Comment.Author>
+                <Comment.Text
+                  style={{
+                    marginTop: "5px",
+                    wordWrap: "break-word",
+                  }}
+                >
+                  {item.text}
+                </Comment.Text>
+                <Divider />
               </Comment.Content>
             </Comment>
           )}
